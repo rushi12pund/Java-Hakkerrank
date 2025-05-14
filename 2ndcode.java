e7cda55f14203fef90b65d207b553b607d25a1ea
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-14 15:00:57 +0530

    Create 2ndcode.java

---
+++ 2ndcode.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+       
+      
+       Scanner Scanner= new Scanner(System.in);
+       
+      
+       int a=Scanner.nextInt();
+       
+       int b=Scanner.nextInt();
+      
+       int c=Scanner.nextInt();
+       Scanner.close();
+       
+       System.out.println( +a);
+       System.out.println( +b);
+        System.out.println( +c);
+    }
+}
