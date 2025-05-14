667f21bd48c355d35af9fb2c359385876b19ee13
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-14 15:46:50 +0530

    Create 3rdcode.java

---
+++ 3rdcode.java
@@ -0,0 +1,37 @@
+//Doing third code on hakker rank
+
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+int n;
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int  n= scanner.nextInt();
+    
+       scanner.close();
+       
+       if(n % 2 != 0){
+        System.out.println("Weird");
+       }else if(n % 2 == 0 && n>=2 && n<=5){
+        System.out.println(" Not Weird");
+        }else if(n % 2 == 0 && n>=6 && n<=20){
+        System.out.println("Weird");
+        } else if(n % 2 == 0 && n>=20){
+        System.out.println("Not Weird");
+        }
+        
+       }
+    
+}
