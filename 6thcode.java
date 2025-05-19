30e965389ffdd72be58a5d8b8398a8f7d0ff8032
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-19 21:21:25 +0530

    Update 6thcode.java

--- 6thcode.java
+++ 6thcode.java
@@ -1,1 +1,24 @@
-///codw 6
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        bufferedReader.close();
+        int temp=0;
+         for(int i=1;i<=10;i++){
+            
+            temp=N*i;
+          System.out.println(N +" x " +i+ " = " +temp);
+        }
+    }
+}
+
